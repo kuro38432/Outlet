cc4ee9d612962546fb76f5de1961aff548f7aef8
SeanWiz <triclopszx@gmail.com>
2017-01-14 15:13:16 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/Animators.meta
    #	Assets/_Scenes/Main - Library.unity

---
+++ Assets/Scripts/SetupOutlet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupOutlet : MonoBehaviour {
+
+    public GameObject[] outlets;
+    int i;
+
+	// Use this for initialization
+	void Start () {
+        i = Random.Range(0, outlets.Length);
+        outlets[i].SetActive(true);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
