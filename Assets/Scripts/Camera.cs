70e38e31ba5ece67fca7ecad318c170d80e7a428
Andrew Law <andrewcylaw@hotmail.com>
2017-01-14 13:38:03 -0500

    Adding man

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public Camera mainCamera;
+    private Vector3 velocity = Vector3.zero;
+
+    // Moves the camera
+    void FixedUpdate()
+    {
+        mainCamera.transform.localPosition = Vector3.SmoothDamp(mainCamera.transform.localPosition, new Vector3(gameObject.transform.position.x, 0.0f, -10.0f), ref velocity, 0.2f);
+    }
+}
+
